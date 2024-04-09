af164cf4e04117fa5f5eb2125b2666d5d5f70564
Wong Her Seng <82885859+MarcusWong01@users.noreply.github.com>
2024-04-09 14:28:21 +0800

    4/9/2024 first change

--- sender.py
+++ sender.py
@@ -2,13 +2,13 @@ import socket
 import cv2
 import base64
 
-def sender_socket(frame):
+# Connect to the C# server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 65536)
+socket_address = ('localhost', 9999)
+server_socket.bind(socket_address)
 
-    # Connect to the C# server
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 65536)
-    socket_address = ('localhost', 9999)
-    server_socket.bind(socket_address)
+def sender_socket(frame):
 
     encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
     message = base64.b64encode(buffer)
